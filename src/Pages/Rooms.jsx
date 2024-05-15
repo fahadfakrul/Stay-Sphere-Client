4e8c1c2c72af77b560e4d1d5a258b7812377524f
Fahad <127460043+fahadfakrul@users.noreply.github.com>
2024-05-15 19:12:32 +0600

    filter added to room

--- src/Pages/Rooms.jsx
+++ src/Pages/Rooms.jsx
@@ -5,14 +5,17 @@ import { Helmet } from "react-helmet";
 
 const Rooms = () => {
   const [rooms, setRooms] = useState([]);
+  const [filter, setFilter] = useState("");
 
   useEffect(() => {
     const getData = async () => {
-      const { data } = await axios(`${import.meta.env.VITE_API_URL}/rooms`);
+      const { data } = await axios(
+        `${import.meta.env.VITE_API_URL}/rooms?filter=${filter}`
+      );
       setRooms(data);
     };
     getData();
-  }, []);
+  }, [filter]);
 
   return (
     <div>
@@ -27,6 +30,22 @@ const Rooms = () => {
             <h2 className="text-4xl font-extrabold mb-4">
               Explore Our Array of Luxurious Rooms for <br /> Your Perfect Stay!
             </h2>
+            <div className="container mx-auto flex  items-center gap-5">
+              <p className="font-semibold text-xl text-gray-400">Filter:</p>
+
+              <select
+                onChange={(e) => setFilter(e.target.value)}
+                value={filter}
+                name="Price"
+                id="Price"
+                className="border p-2 rounded-md "
+              >
+                <option value="">Sort By Price</option>
+                <option value="0-200">$ 0 - $ 200</option>
+                <option value="201-400">$ 201 - $ 400</option>
+                <option value="401-600">$ 401 - $ 600</option>
+              </select>
+            </div>
           </div>
           <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-3">
             {rooms.map((room) => (
